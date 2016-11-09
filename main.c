ea7931f47985b5b6f119a5f78691b313388fd8b7
BangUl_Pro <wind.dong.dream@gmail.com>
2016-11-09 09:01:44 +0900

    파일 생성

--- main.c
+++ main.c
@@ -1,15 +1,6 @@
-//
-//  main.c
-//  Library
-//
-//  Created by 이동규 on 2016. 10. 14..
-//  Copyright © 2016년 이동규. All rights reserved.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void) {
+    
     return 0;
 }
