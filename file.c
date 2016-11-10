bf5a4863d26f4dff8fbdb1210b372af22ee1c036
BangUl_Pro <wind.dong.dream@gmail.com>
2016-11-10 14:08:25 +0900

    File load 동적 할당

--- file.c
+++ file.c
@@ -9,9 +9,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "file.h"
+#include "list.h"
 
 
-void loadFile(client *clients, book *books, borrow *borrows) {
+void loadFile(clientNode *clients, bookNode *books, borrowNode *borrows) {
     // 파일 이름
     char clientFileName[] = "./client.txt";
     char bookFileName[] = "./book.txt";
@@ -43,8 +44,27 @@ void loadFile(client *clients, book *books, borrow *borrows) {
     // client 파일 데이터 추출
     while (!feof(clientFile)) {
         // TODO - 동적 생성 수정해야함
-        clients = calloc(1, sizeof(client));
-        fscanf(clientFile, "%d||%[^|]||%[^|]||%[^|]||%s\n", &(clients[i].studentId), clients[i].password, clients[i].name, clients[i].address, clients[i].phone);
+        clientNode* node = (clientNode *) calloc(1, sizeof(clientNode));
+        fscanf(clientFile, "%d||%[^|]||%[^|]||%[^|]||%s\n", &(node -> client.studentId), node -> client.password, node -> client.name, node -> client.address, node -> client.phone);
+        addClient(clients, node);
+        i++;
+    }
+    
+    // book 파일 데이터 추출
+    while (!feof(bookFile)) {
+        // TODO - 동적 생성 수정해야함
+        bookNode* node = (bookNode *) calloc(1, sizeof(bookNode));
+        fscanf(bookFile, "%d||%[^|]||%[^|]||%[^|]||%ld||%[^|]||%c\n", &(node -> book.bookId), node -> book.name, node -> book.publisherName, node -> book.authorName, &(node -> book.ISBN), node -> book.holdingInstitudtion, &(node -> book.isBorrowable));
+        addBook(books, node);
+        i++;
+    }
+    
+    // borrow 파일 데이터 추출
+    while (!feof(borrowFile)) {
+        // TODO - 동적 생성 수정해야함
+        borrowNode* node = (borrowNode *) calloc(1, sizeof(borrowNode));
+        fscanf(borrowFile, "%d||%d||%ld||%ld\n", &(node -> borrow.studentId), &(node -> borrow.bookId), &(node -> borrow.borrowDateSec), &(node -> borrow.returnDateSec));
+        addBorrow(borrows, node);
         i++;
     }
     
