935ed26dcd40c076e5e48f75889dab2d7b39c730
BangUl_Pro <wind.dong.dream@gmail.com>
2016-11-10 14:08:25 +0900

    File load 동적 할당

--- main.c
+++ main.c
@@ -1,6 +1,24 @@
 #include "list.h"
+#include <stdlib.h>
 
 int main(void) {
+    clientNode *clientList = calloc(1, sizeof(clientNode)), a, b;
+    bookNode *bookList = calloc(1, sizeof(bookNode));
+    borrowNode *borrowList = calloc(1, sizeof(borrowNode));
+    loadFile(clientList, bookList, borrowList);
+    
+    for (int i = 0; i < clientLength(*clientList); i++) {
+        printf("%d\n", getClientNode(clientList, i) -> client.studentId);
+    }
+    
+    for (int i = 0; i < bookLength(*bookList); i++) {
+        printf("%d\n", getBookNode(bookList, i) -> book.bookId);
+    }
+    
+    for (int i = 0; i < borrowLength(*borrowList); i++) {
+        printf("Book = %d\n", getBorrowNode(borrowList, i) -> borrow.bookId);
+        printf("Student = %d\n", getBorrowNode(borrowList, i) -> borrow.studentId);
+    }
     
     return 0;
 }
